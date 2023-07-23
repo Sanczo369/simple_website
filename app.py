f336cf16dd931cea47bb18a1978c46171173defa
Arek Sanecki <areksanecki@gmail.com>
2023-07-23 23:46:52 +0200

    dodanie klasy FlaskForm ContactForm

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm 
+from wtforms import StringField, EmailField, TextAreaField,TelField
 
 app = Flask(__name__) 
+
+
+class ContactForm(FlaskForm):
+    name = StringField('Name')
+    email = EmailField('Email')
+    phone = TelField('Phone')
+    text = TextAreaField('Text')
+
+
+
+
+
+
 @app.route('/') 
 def index(): 
     return render_template('index.html')
