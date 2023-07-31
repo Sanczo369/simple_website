3b5751ffcba725efc3acc3afa97b023893f73dbd
Arek Sanecki <areksanecki@gmail.com>
2023-07-31 16:17:34 +0200

    dodanie flask_mail oraz validator do formularza

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, flash, redirect
 from flask_wtf import FlaskForm 
 from wtforms import StringField, EmailField, TextAreaField,TelField
+from flask_mail import Mail, Message
+from wtforms.validators import DataRequired
 
 app = Flask(__name__) 
 # konfiguracja
 app.config.from_pyfile('config.cfg')
-
+mail = Mail(app)
 class Contact:
     def __init__(self, name, phone, email, text):
         self.name = name
@@ -14,21 +16,20 @@ class Contact:
         self.text=text
         
 class ContactForm(FlaskForm):
-    name = StringField('Name')
-    email = EmailField('Email')
-    phone = TelField('Phone')
-    text = TextAreaField('Text')
-
-
-
-
-
-
-@app.route('/') 
+    name = StringField('Name', validators=[DataRequired()])
+    email = EmailField('Email', validators=[DataRequired()])
+    phone = TelField('Phone', validators=[DataRequired()])
+    text = TextAreaField('Text', validators=[DataRequired()])
+    
+@app.route('/', methods=['GET', 'POST'])
 def index(): 
     contact = Contact(name="Jan Nowak", email='email@email.pl', phone=111, text= 'text')
     form = ContactForm()
-    return render_template('index.html', form=form)
+    if request.method == 'POST' and form.validate_on_submit():
+        return render_template('success.html', )  # Przekierowanie na nowy widok dla sukcesu
+    else:    
+        return render_template('index.html', form=form)
+
 
 if __name__ == '__main__': 
     app.run()
