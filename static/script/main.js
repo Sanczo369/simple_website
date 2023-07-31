438c9c0316b76901fb9e7544cc55fe4985229f0a
Arek Sanecki <areksanecki@gmail.com>
2023-07-31 13:00:52 +0200

    animacja sekcji nav

--- static/script/main.js
+++ static/script/main.js
@@ -1,10 +1,13 @@
-const toTop = document.querySelector('.to-top');
+const toTop = document.querySelector('.navbar');
+const navBar = document.querySelector('.to-top');
 
 window.addEventListener('scroll', () =>{
     if (window.scrollY>100){
         toTop.classList.add("active");
+        toTop.classList.add("activeNav");
     }
     else{
         toTop.classList.remove("active");
+        toTop.classList.remove("activeNav");
     }
 })
