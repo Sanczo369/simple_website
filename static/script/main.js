26143b46e584ca165571b3639feb7cb26a4ca350
Arek Sanecki <areksanecki@gmail.com>
2023-08-03 00:04:25 +0200

    dodanie skryptu sekcji opinions

--- static/script/main.js
+++ static/script/main.js
@@ -43,3 +43,12 @@ valueDisplays.forEach((valueDisplay) => {
         }
     }, duration)
 });
+
+let counter = 1;
+setInterval(function(){
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if(counter > 4){
+        counter = 1;
+    }
+}, 5000);
