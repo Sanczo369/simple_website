8a846beb069d68de781d4c144b812a444d9e575f
Arek Sanecki <areksanecki@gmail.com>
2023-08-07 13:03:31 +0200

    przeniesienie recipients do config.cfg

--- app.py
+++ app.py
@@ -34,31 +34,32 @@ def index():
     db.create_all() 
     form = ContactForm()
     newsform = NewsletterForm()
-    if newsform.validate_on_submit():
-        email = newsform.email.data
-        logging.info(f"Formularz wysłany przez: ({email})")
-        new_email = Newsletter(email=email)
-        logging.error(f"Utworzenie nowego rekordu: ({email})")
-        db.session.add(new_email)
-        logging.error(f"Dodanie nowego rekordu: ({email})")
-        db.session.commit()
-        logging.error(f"zapisanie nowego rekordu: ({email})")
+    if request.method == 'POST':
+        if newsform.validate_on_submit():
+            email = newsform.email.data
+            logging.info(f"Formularz wysłany przez: ({email})")
+            new_email = Newsletter(email=email)
+            logging.error(f"Utworzenie nowego rekordu: ({email})")
+            db.session.add(new_email)
+            logging.error(f"Dodanie nowego rekordu: ({email})")
+            db.session.commit()
+            logging.error(f"zapisanie nowego rekordu: ({email})")
+            
+        if form.validate_on_submit():
+            name = form.name.data
+            email = form.email.data
+            phone = form.phone.data
+            text = form.text.data
         
-    if request.method == 'POST' and form.validate_on_submit():
-        name = form.name.data
-        email = form.email.data
-        phone = form.phone.data
-        text = form.text.data
-    
-        
-        # Tworzenie wiadomości email
-        subject = "Wiadomość od {} ({})".format(name, email)
-        body = "Wiadomość: {}\nTelefon: {}".format(text, phone)
-        message = Message(subject=subject, body=body, recipients=["test.test.test@vp.pl"])
-        
-        # Wysyłanie wiadomości
-        mail.send(message)
-        return redirect('/')
+            
+            # Tworzenie wiadomości email
+            subject = "Wiadomość od {} ({})".format(name, email)
+            body = "Wiadomość: {}\nTelefon: {}".format(text, phone)
+            message = Message(subject=subject, body=body, recipients=app.config.get("MAIL_RECIPIENTS", []))
+            
+            # Wysyłanie wiadomości
+            mail.send(message)
+            return redirect('/')
     
     return render_template('index.html', form=form , newsform=newsform)
 
